02a35d6b27dee4a9bc68c8ad3f3dd7de8c556e36
Adam Daly <adamdaly@rehabstudio.com>
2014-08-05 17:14:10 +0100

    updating API_ENDPOINT in base-collection.js

--- js/tests/test.js
+++ js/tests/test.js
@@ -5,20 +5,35 @@ var testDataTracks = require('./data/tracks');
 var mockjax = require('../../node_modules/jquery-mockjax/jquery.mockjax');
 
 $.mockjax({
-	url: 'http://rehabradio.vagrant.local:8000/api/queue',
+	url: 'http://localhost:8000/api/queue',
 	responseTime: 750,
 	responseText: testDataQueue
 });
 
 $.mockjax({
-	url: 'http://rehabradio.vagrant.local:8000/api/playlists',
+	url: 'http://localhost:8000/api/playlists',
 	responseTime: 750,
 	responseText: testDataPlaylists
 });
 
 $.mockjax({
-	url: /(http:\/\/rehabradio.vagrant.local:8000\/api\/playlists\/)([\d]+)/,
-	urlParams: ['root', 'playlistId'],
+	url: /http:\/\/localhost:8000\/api\/playlists\/([\d]+)/,
+	urlParams: ['playlistId'],
+	responseTime: 750,
+	response: function(settings){
+
+		var playlistId = parseInt(settings.urlParams.playlistId),
+			data = _.findWhere(testDataTracks, {id: playlistId });
+
+		this.responseText =  JSON.stringify(data);
+	}
+});
+
+// "http://rehabradio.localhost/api/metadata/tracks/add/playlist/?source_type=spotify&source_id=5hfP8cKSzpfBisP3R24cAy&playlist_id=2". 
+
+$.mockjax({
+	url: /http:\/\/localhost:8000\/api\/metadata\/tracks\/add\/playlist\/([\d]+)/,
+	urlParams: ['playlistId'],
 	responseTime: 750,
 	response: function(settings){
 
