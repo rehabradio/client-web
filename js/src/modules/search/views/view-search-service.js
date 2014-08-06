50e3986214f79907b2586d92ed23a1d6cdd62981
Daniel Skelton <daniel.skelton@rehabstudio.com>
2014-08-06 15:50:26 +0100

    added basic pagination for search results

--- js/src/modules/search/views/view-search-service.js
+++ js/src/modules/search/views/view-search-service.js
@@ -1,36 +1,59 @@
 var SearchCollection = require('../collections/collections-search');
 var SearchTrackView = require('./view-search-track');
+var _ = require('underscore');
 
 module.exports = Backbone.View.extend({
 
 	el: null,
 	tagName: 'li',
 
+	events: {
+		'click .pagination a' : 'paginate'
+	},
+
+	pagination_template: require('../templates/pagination.hbs'),
+
 	initialize: function(options){
 
-		console.log(this.$el);
-
-		console.log('SearchServiceView::initialize');
-
 		this.service = options.service;
-
 		this.collection = new SearchCollection();
-
 		this.listenTo(this.collection, 'add', this.addTrack, this);
 
 		dispatcher.on('perform-search', this.performSearch, this);
-
 	},
 
+	updatePagination:function(resp){
+		this.$el.find('.pagination').html( this.pagination_template({ 
+			next: resp.next,
+			prev: resp.previous
+		}));
+	},
+
+	paginate:function(event){
+
+		event.preventDefault();
+		this.search({ url: event.currentTarget.href });
+	},
+
+	search:function(options){
+
+		this.$el.find('.results').empty();
+
+		var fetch = this.collection.fetch(options);
+		fetch.done(this.updatePagination.bind(this));
+
+	},	
+
 	performSearch:function(payload){
-		this.$el.empty();
-		this.collection.fetch({service: this.service, query: payload});
+
+		this.search({service: this.service, query: payload});
+
 	},	
 
 	addTrack:function(track){
 
 		var trackView = new SearchTrackView({model: track});
 
-		this.$el.append(trackView.render().$el);
+		this.$el.find('.results').append(trackView.render().$el);
 	}
 });
