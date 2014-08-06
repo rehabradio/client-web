2b6c6d2f74fbf9033300057b88b8fb36f95624cd
Daniel Skelton <daniel.skelton@rehabstudio.com>
2014-08-06 17:52:09 +0100

    wip search module - spotify pagination

--- js/src/modules/search/views/view-search.js
+++ js/src/modules/search/views/view-search.js
@@ -18,20 +18,14 @@ var SearchView = Backbone.View.extend({
 	views: [],
 
 	initialize: function(){
-		console.log('SearchView::initialize');
-
 		for(var service in this.services){
-			this.views.push( new SearchServiceView({
-				el: this.services[service], service: service
-			}));
+			this.views.push( new SearchServiceView({ el: this.services[service], service: service }) );
 		}
 	},
 
 	onServiceSelect:function(e){
-
 		var service = e.currentTarget.name;
 		this.services[service] == e.currentTarget.checked;
-
 	},
 
 	onSubmit:function(event){
