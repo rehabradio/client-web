b715b8c21bb743c0c3d53507caf4d8586129ede1
Adam Daly <adamdaly@rehabstudio.com>
2014-08-08 15:08:41 +0100

    combining playlists and tracks modules

--- js/src/utils/dataStore.js
+++ js/src/utils/dataStore.js
@@ -7,10 +7,10 @@ var dataStore = {}
 var PlaylistsCollection = require('../../../modules/playlists/collections/collections-playlists');
 dataStore.playlistsCollection = new PlaylistsCollection();
 
+var TracksCollection = require('../../../modules/playlists/collections/collections-tracks');
+dataStore.tracksCollection = new TracksCollection();
+
 var QueueCollection = require('../../../modules/queue/collections/collection-queue');
 dataStore.queueCollection = new QueueCollection();
-
-var TracksCollection = require('../../../modules/tracks/collections/collections-tracks');
-dataStore.tracksCollection = new TracksCollection();
 
 module.exports = dataStore;
