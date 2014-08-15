89e1f50e686b3ed5ffcb81d42f8eecf2efc6cefb
Daniel Skelton <daniel.skelton@rehabstudio.com>
2014-08-15 12:29:14 +0100

    url must have a trailing slash else preflight requests fail

--- modules/core/base-collection.js
+++ modules/core/base-collection.js
@@ -1,4 +1,5 @@
 var Backbone = require('backbone');
+var AuthToken = require('./tempAuth');
 
 var BaseCollection = Backbone.Collection.extend({
 
@@ -7,7 +8,7 @@ var BaseCollection = Backbone.Collection.extend({
 	model: null,
 
 	url: function(){
-		return this.API_ENDPOINT + this.request;
+		return this.API_ENDPOINT + this.request + "/";
 	},
 	
 	parse: function(res){
@@ -15,14 +16,9 @@ var BaseCollection = Backbone.Collection.extend({
 	},
 
 	sync:function(method, model, options){
-
 		options.beforeSend = function (xhr) {
-			xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    		xhr.setRequestHeader('X_GOOGLE_AUTH_TOKEN', 'ACCESS_TOKEN');
+    		xhr.setRequestHeader('X_GOOGLE_AUTH_TOKEN', AuthToken);
   		};
-
-  		console.log(arguments);
-
   		return Backbone.Collection.prototype.sync.apply(this, arguments);	
 	}
 
