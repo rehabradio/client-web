4962a83aeb4f3c7960b5d0a0afc97c813bae5d65
Adam Daly <adamdaly@rehabstudio.com>
2014-08-18 10:12:49 +0100

    changed api-root to localhost

--- js/app.js
+++ js/app.js
@@ -42,7 +42,8 @@ if(document.location.search.match(/debug/gi)){
  *	Define and initialise the root view to start the application
  */
 
-window.API_ROOT = 'http://server-core.herokuapp.com/api/';
+// window.API_ROOT = 'https://server-core.herokuapp.com/api/';
+window.API_ROOT = 'http://localhost:8000/api/';
 
 var AppView = require('../modules/app/views/view-app');
 
