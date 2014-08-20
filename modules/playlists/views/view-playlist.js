2392742f71ed47e3779bc3b001b2623dac06cf9f
Adam Daly <adamdaly@rehabstudio.com>
2014-08-20 17:32:24 +0100

    images for cover art where available. fix for wrong id being passed to queue:add

--- modules/playlists/views/view-playlist.js
+++ modules/playlists/views/view-playlist.js
@@ -2,9 +2,43 @@ module.exports = Marionette.ItemView.extend({
 
 	template: require('../templates/view-playlist.hbs'),
 
+	className: 'playlist',
+
 	events: {
 		'click .playlist-view': '_onShowPlaylist',
 		'click .playlist-delete': '_onDeletePlaylist'
+	},
+
+	initialize: function(){
+		var self = this;
+
+		self.listenTo(self.model, 'change:coverart', self._onCoverartChange);
+
+		$.ajax({
+			url: window.API_ROOT + 'playlists/' + self.model.get('id') + '/tracks/',
+			type: 'GET',
+			success: function(data){
+
+				var count = Math.min(data.count, 4),
+					coverart = [];
+
+				for(var i = 0; i < count; i++){
+
+					if(!!data.results[i]){
+
+						if(!!data.results[i].track.image_small){
+
+							coverart.push(data.results[i].track.image_small);
+						}else{
+
+							count++;
+						}
+					}
+				}
+
+				self.model.set('coverart', coverart);
+			}
+		});
 	},
 
 	_onShowPlaylist: function(){
@@ -16,5 +50,14 @@ module.exports = Marionette.ItemView.extend({
 	_onDeletePlaylist: function(){
 
 		dispatcher.trigger('playlist:delete', this.model);
+	},
+
+	_onCoverartChange: function(){
+		
+		var self = this;
+
+		_.each(this.model.get('coverart'), function(element){
+			self.$el.find('.coverart').append('<img src="' + element + '" alt="">');
+		});
 	}
 });
