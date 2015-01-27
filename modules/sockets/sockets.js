72063789922b4f881c71cbb27e655df7e0545ae0
Adam Daly <adamdaly@rehabstudio.com>
2015-01-27 14:58:20 +0000

    reverted to basic websocket script

--- modules/sockets/sockets.js
+++ modules/sockets/sockets.js
@@ -1,36 +1,42 @@
 var io = require('socket.io-client'),
 	socket = io.connect(process.env.SOCKETS_URL);
 
-socket.on('connection', function(data){
-	debugger;
-})
+// socket.on('connection', function(data){
+// 	debugger;
+// });
 
 function Sockets(){
 
-	socket.on('queues:updated', function(){
+	websocket = new WebSocket(process.env.SOCKETS_URL);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+
+	// socket.on('queues:updated', function(){
 		
-		dispatcher.trigger('socket:queues:updated');
-	});
+	// 	dispatcher.trigger('socket:queues:updated');
+	// });
 
-	socket.on('queue:updated', function(data){
+	// socket.on('queue:updated', function(data){
 		
-		// dispatcher.trigger('socket:queues:tracks:updated', data);
-	});
+	// 	// dispatcher.trigger('socket:queues:tracks:updated', data);
+	// });
 
-	socket.on('playlists:updated', function(data){
+	// socket.on('playlists:updated', function(data){
 		
-		// dispatcher.trigger('socket:playlists:updated', data);
-	});
+	// 	// dispatcher.trigger('socket:playlists:updated', data);
+	// });
 
-	socket.on('playlist:updated', function(data){
+	// socket.on('playlist:updated', function(data){
 		
-		// dispatcher.trigger('socket:playlists:tracks:updated', data);
-	});
+	// 	// dispatcher.trigger('socket:playlists:tracks:updated', data);
+	// });
 
 
-	socket.on('queue-head:updated', function(data){
-		// dispatcher.trigger('socket:player:set', data);
-	});
+	// socket.on('queue-head:updated', function(data){
+	// 	// dispatcher.trigger('socket:player:set', data);
+	// });
 }
 
 module.exports = new Sockets();
