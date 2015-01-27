5c4f4fe7a7989e76bbd0179323cae9f8d6adf2eb
Adam Daly <adamdaly@rehabstudio.com>
2015-01-27 15:51:56 +0000

    added in ajax login pre sockets connection

--- modules/sockets/sockets.js
+++ modules/sockets/sockets.js
@@ -1,12 +1,9 @@
-var io = require('socket.io-client'),
-	socket = io.connect(process.env.SOCKETS_URL);
-
-// socket.on('connection', function(data){
-// 	debugger;
-// });
+var io = require('socket.io-client');
 
 function Sockets(){
+	var login = this.login();
 
+	login.done(this.onLoginDone);
 	// socket.on('queues:updated', function(){
 		
 	// 	dispatcher.trigger('socket:queues:updated');
@@ -31,6 +28,34 @@ function Sockets(){
 	// socket.on('queue-head:updated', function(data){
 	// 	// dispatcher.trigger('socket:player:set', data);
 	// });
+}
+
+Sockets.prototype = {
+
+	login: function(){
+		return $.ajax({
+			url: 'http://rehabradio.herokuapp.com/login',
+			type: 'GET'
+		});
+	},
+
+	onLoginDone: function(data){
+		debugger;
+
+		namespace = '/updates';
+    	socket = io.connect('wss://radio-socket-server.herokuapp.com/');
+
+    	socket.on('connect', this.onConnection);
+
+	},
+
+	onConnection: function(){
+		debugger;
+	},
+
+	onError: function(){
+		debugger;
+	}
 }
 
 module.exports = new Sockets();
