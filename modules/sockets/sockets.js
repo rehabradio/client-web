4787e79443f811ca856efce8a2749138390c7e53
Adam Daly <adamdaly@rehabstudio.com>
2015-01-28 12:53:58 +0000

    switched to socket.io syntax

--- modules/sockets/sockets.js
+++ modules/sockets/sockets.js
@@ -46,18 +46,18 @@ Sockets.prototype = {
 	onLoginDone: function(data){
 
 		// namespace = '/updates';
-    	// socket = io.connect('wss://radio-socket-server.herokuapp.com', {'force new connection': true});
+    	socket = io.connect('wss://radio-socket-server.herokuapp.com', {'force new connection': true});
 
-    	// socket.on('message', this.onMessage);
-    	// socket.on('connect', this.onConnect);
-    	// socket.on('disconnect', this.onDisconnect);
-    	// socket.on('error', this.onError);
+    	socket.on('message', this.onMessage);
+    	socket.on('connect', this.onConnect);
+    	socket.on('disconnect', this.onDisconnect);
+    	socket.on('error', this.onError);
 
-    	var socket = new WebSocket('wss://radio-socket-server.herokuapp.com');
-    	socket.onopen = this.onConnect;
-    	socket.onmessage = this.onMessage;
-    	socket.onclose = this.onDisconnect;
-    	socket.onerror = this.onError;
+    	// var socket = new WebSocket('wss://radio-socket-server.herokuapp.com');
+    	// socket.onopen = this.onConnect;
+    	// socket.onmessage = this.onMessage;
+    	// socket.onclose = this.onDisconnect;
+    	// socket.onerror = this.onError;
 
 	},
 
